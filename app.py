d057831f9af8ecd0b31d9e51a1da674553a1bf66
soori <tharukaravisra@gmail.com>
2023-12-02 21:08:18 +0530

    Implement user login, enhance registration endpoint, and develop charging station listing
    - Added user login functionality with JWT token creation
    - Enhanced user registration endpoint with better error handling
    - Implemented charging station listing endpoint
    - Created additional routes for user profile and general error handling
    - Refactored and organized code for better readability and maintainability

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
-from flask import Flask, render_template
+# app.py
+from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
-from flask import request, jsonify, redirect, url_for
-from models import User, ChargingStation
-from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+from flask_migrate import Migrate
 
+# Initialize Flask app
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@localhost/mydatabase'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://@localhost:5432/CylonEchoPlug'
 app.config['SECRET_KEY'] = 'MySecretKey'
+
+# Initialize extensions
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
+
+# Initialize Flask-Migrate
+migrate = Migrate(app, db)
+
+# Import models
+from models import User, ChargingStation
 
 
 @app.route('/')
@@ -17,18 +27,17 @@ def home():
     return render_template('home.html')
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
-
-
-@app.route('register', methods=['POST'])
+@app.route('/register', methods=['POST'])
 def register():
-    data = request.get_json()
-    hashed_password = bcrypt.generate_password_hash(data['password']).decode('utf-8')
-    new_user = User(username=data['username'], email=data['email'], password=hashed_password)
-    db.session.add(new_user)
-    db.session.commit()
-    return jsonify({'message': 'New user created!'}), 201
+    try:
+        data = request.get_json()
+        hashed_password = bcrypt.generate_password_hash(data['password']).decode('utf-8')
+        new_user = User(username=data['username'], email=data['email'], password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify({'message': 'User registered successfully'}), 201
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 
 @app.route('/login', methods=['POST'])
@@ -44,9 +53,9 @@ def login():
 
 
 @app.route('/charging_stations', methods=['POST'])
-@jwt_required
+@jwt_required()
 def register_charging_station():
-    data = request.get_jso()
+    data = request.get_json()
     new_charging_station = ChargingStation(
         name=data['name'],
         location=data['location'],
@@ -54,8 +63,39 @@ def register_charging_station():
         availability=data['availability'],
         pricing=data['pricing'],
         opening_hours=data['opening_hours'],
-        operator_id=data['operator_id']
+        operator_id=get_jwt_identity()
     )
     db.session.add(new_charging_station)
     db.session.commit()
     return jsonify({'message': 'Charging station registered successfully'}), 201
+
+
+@app.route('/user_profile', methods=['GET'])
+@jwt_required()
+def profile():
+    current_user_id = get_jwt_identity()
+    user = User.query.get(current_user_id)
+    user_data = {
+        'id': user.id,
+        'username': user.username,
+        'email': user.email,
+        'charging_stations': [{'name': station.name, 'location': station.location} for station in
+                              user.charging_stations]
+    }
+    return jsonify({'user': user_data}), 200
+
+
+@app.route('/charging_stations', methods=['GET'])
+def charging_stations():
+    stations = ChargingStation.query.all()
+    station_list = [{'name': station.name, 'location': station.location} for station in stations]
+    return jsonify(station_list), 200
+
+
+@app.errorhandler(404)
+def not_found(_):
+    return jsonify({'error': 'Not found'}), 404
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+# models.py
 from app import db
 from datetime import datetime
 
@@ -8,6 +9,7 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    charging_stations = db.relationship('ChargingStation', backref='operator', lazy=True)
 
     def __repr__(self):
         return f'<User {self.username}>'
@@ -17,13 +19,11 @@ class ChargingStation(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
     location = db.Column(db.String(255), nullable=False)
-    Charger_types = db.Column(db.String(255), nullable=False)
+    charger_types = db.Column(db.String(255), nullable=False)
     availability = db.Column(db.Boolean, default=True)
     pricing = db.Column(db.String(255), nullable=False)
     opening_hours = db.Column(db.String(255))
-    operator_id = db.Column(db.Integer, db.ForeignKey('operator.id'), nullable=False)
+    operator_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
         return f'<ChargingStation {self.name}>'
-
-
